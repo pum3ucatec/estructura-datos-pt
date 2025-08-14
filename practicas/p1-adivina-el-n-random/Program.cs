20d76da61701baa04796a079f054b78e53832842
coki141621 <cokicoki233@gmail.com>
2025-08-14 14:57:08 -0400

    respaldo de mis ejercicios

---
+++ practicas/p1-adivina-el-n-random/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 51);
+        int intento;
+        int intentos = 0;
+
+        Console.WriteLine("Adivina el número (entre 1 y 50):");
+
+        do
+        {
+            Console.Write("Tu intento: ");
+            intento = int.Parse(Console.ReadLine());
+            intentos++;
+
+            if (intento < numeroSecreto)
+                Console.WriteLine("Muy bajo");
+            else if (intento > numeroSecreto)
+                Console.WriteLine("Muy alto");
+
+        } while (intento != numeroSecreto);
+
+        Console.WriteLine($"¡Correcto! Lo lograste en {intentos} intentos.");
+    }
+}
+
---
+++ practicas/p1-validar-entrada/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        do
+        {
+            Console.Write("Ingrese un número positivo: ");
+            numero = int.Parse(Console.ReadLine());
+        } while (numero <= 0);
+
+        Console.WriteLine($"Número válido ingresado: {numero}");
+    }
+}
