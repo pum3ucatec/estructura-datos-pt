c95dfb59ac505970a7c86bc2209156bbb24adb40
Isabel <quisperamosisabelalejandra@gmail.com>
2025-08-14 12:28:58 -0400

    respaldo de mis ejercicios

--- practicas/p1-contar-1-al-10/Program.cs
+++ practicas/p1-contar-1-al-10/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, isa!");
+using System;
+
+namespace ContarDel1Al10
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+  
---
+++ practicas/p2-sumar-numeros-del-1-al-N/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Sumar_numeros_del_1alN
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N, suma = 0, contador = 1;
+
+            Console.Write("Ingrese un número N: ");
+            N = int.Parse(Console.ReadLine());
+
+            while (contador <= N)
+            {
+                suma += contador;
+                contador++;
+            }
+
+            Console.WriteLine($"La suma de los números del 1 al {N} es: {suma}");
+        }
+    }
+}
