389c47c2aaa6bcba5a21bf555917d50f087ebc67
LunaDidi23 <lauradiana7620@gmail.com>
2025-08-14 17:50:17 -0400

    Subiendo mi primer ejercicio

---
+++ practicas/p5-contador-regresivo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 30;
+            while (c > 0)
+            {
+                Console.WriteLine(c);
+                c--;
+            }
+        }
+    }
+}
