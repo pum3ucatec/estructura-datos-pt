c0b9c0d5d4bf184060d129fd5508dc73749be572
PaolaLV71 <paolalaure71@gmail.com>
2025-08-15 10:44:44 -0400

    Microevaluacion1

---
+++ examenes/microevaluacion1/Respuesta.md
@@ -0,0 +1,186 @@
+# GUIA: ENTORNO DE DESARROLLO
+
+## VS Code, Git, GitHub, C#
+
+### 1. Instalacion VS Code
+
+- Descarga VS Code: **[Download VS Code](https://code.visualstudio.com/download)**
+
+- Instalar con la configuracion predeterminada
+- Instalar extensiones:
+
+  - **_C# Dev Kit by Microsoft_**
+
+---
+
+### 2. Instalacion de Git
+
+- Descarga Git: **[Download Git](https://git-scm.com/downloads)**
+- Instalar con la configuracion predeterminada
+- Prueba de version
+
+---
+
+<pre> git --version </pre>
+
+---
+
+### 3. Instalacion Dotnet
+
+- Descargar Dotnet: **[Download Dotnet](https://dotnet.microsoft.com/en-us/download)**
+- Instalar con la configuracion predeterminada
+- Prueba de version
+
+---
+
+<pre> dotnet --version</pre>
+
+---
+
+### 4. Configuracion Git y clave SSH
+
+- Configuracion inicial Git
+
+---
+
+<pre>git config --global user.name "Tu nombre"
+
+git config --global user.email "tu_correo@example.com"</pre>
+
+---
+
+Usa el mismo correo que tienes en **[GitHub](https://github.com/login)**
+
+---
+
+#### 4.1. Generar y agragar clave SSH a GitHub
+
+- Generar clave SSH
+
+---
+
+<pre>ssh-keygen -t ed25519 -C "tu_correo_de_github@hotmail.com"
+</pre>
+
+---
+
+- Levantar al agente SSH y agregar una clave
+
+---
+
+<pre>eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+</pre>
+
+---
+
+- Copiar la clave publica
+
+---
+
+<pre>cat ~/.ssh/id_ed25519.pub</pre>
+
+---
+
+**_¡¡¡Copia su contenido!!!_**
+
+---
+
+- Agregar la clave a GitHub
+
+  - Ir a **[GitHub → Settings → SSH and GPG keys](https://github.com/settings/keys)**
+  - Click en **New SSH key** y pega el contenido y guardalo
+
+### 5. Clonar un repositorio de GitHub
+
+- Crea una carpeta para clonar ahi tu repositorio
+
+---
+
+<pre>
+mkdir "nombre_de_tu_carpeta"
+cd nombre_de_tu_carpeta
+</pre>
+
+---
+
+- Clonar un repositorio
+
+---
+
+<pre>git clone git@github.com:usuario/repositorio.git
+
+cd repositorio</pre>
+
+---
+
+### 6. Crear y cambiar de rama
+
+- Crear una rama
+  ***
+  <pre>git branch nombre_de_la_rama
+  </pre>
+  ***
+- Crear una rama y cambiarte a ella
+
+---
+
+ <pre>git checkout -b nombre_rama
+</pre>
+
+---
+
+- Cambiarte a la nueva rama
+
+---
+
+ <pre>git checkout nombre_rama
+</pre>
+
+---
+
+### 7. Crear y ejecutar un proyecto en C#
+
+- Crear una carpeta que vaya a contener el proyecto
+- Abre desde la terminal
+- En la terminal:
+
+  - `dotnet new console`
+
+- Para ejecutar:
+
+  - `dotnet run`
+
+### 8. Subir cambios a GitHub
+
+- Ver estado
+
+---
+
+<pre>git status</pre>
+
+- Agregar todos los cambios
+
+---
+
+<pre>git add .</pre>
+
+- Crear un commit
+
+---
+
+<pre>
+git commit -m "mensaje con los cambios realizados"
+</pre>
+
+---
+
+- Subir los cambios a GitHub
+
+---
+
+<pre>
+git push
+</pre>
+
+---
---
+++ examenes/microevaluacion1/calculadora-cientifica/Program.cs
@@ -0,0 +1,103 @@
+﻿﻿using System;
+
+
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int opcion;
+        do
+        {
+            Console.WriteLine("Menu de opciones:");
+            Console.WriteLine("1.Sumar");
+            Console.WriteLine("2.Restar");
+            Console.WriteLine("3.Multiplicar");
+            Console.WriteLine("4.Dividir");
+            Console.WriteLine("5.Raíz cuadrada");
+            Console.WriteLine("6.Elevado");
+            Console.WriteLine("7.Seno");
+            Console.WriteLine("0.Salir");
+            Console.WriteLine("Seleccione una opcion:");
+            opcion = Convert.ToInt16(Console.ReadLine());
+            double num1, num2, resultado;
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Ingrese el primer numero:");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero:");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    resultado = num1 + num2;
+                    Console.WriteLine($"Resultado: {resultado}");
+                    break;
+                case 2:
+                    Console.WriteLine("Ingrese el primer numero:");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero:");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    resultado = num1 - num2;
+                    Console.WriteLine($"Resultado: {resultado}");
+                    break;
+                case 3:
+                    Console.WriteLine("Ingrese el primer numero:");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero:");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    resultado = num1 * num2;
+                    Console.WriteLine($"Resultado: {resultado}");
+                    break;
+                case 4:
+                    Console.WriteLine("Ingrese el primer numero:");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero:");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Error: Complex Infinity");
+                    }
+                    else
+                    {
+
+                        resultado = num1 / num2;
+                        Console.WriteLine($"Resultado: {resultado}");
+                    }
+                    break;
+                case 5:
+                    Console.WriteLine("Ingrese un numero para calcular su raiz cuadrada:");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    if (num1 < 0)
+                    {
+                        Console.WriteLine("Error: No se puede calcular la raíz cuadrada de un número negativo.");
+                    }
+                    else
+                    {
+                        resultado = Math.Sqrt(num1);
+                        Console.WriteLine($"Raíz cuadrada: {resultado}");
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("Ingrese un número base:");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Ingrese el exponente:");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    resultado = Math.Pow(num1, num2);
+                    Console.WriteLine($"Resultado de {num1} elevado a {num2}: {resultado}");
+                    break;
+                case 7:
+                    Console.WriteLine("Ingrese un ángulo en grados para calcular su seno:");
+                    double anguloSeno = Convert.ToDouble(Console.ReadLine());
+                    double seno = Math.Sin(anguloSeno * Math.PI / 180);
+                    Console.WriteLine($"Seno: {seno}");
+                    break;
+                case 0:
+                    Console.WriteLine("Saliendo...");
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida.");
+                    break;
+            }
+                
+
+            } while (opcion != 0);
+        }
+    }
