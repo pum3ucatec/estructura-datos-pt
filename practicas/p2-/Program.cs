f1f56d7ff923eab34cc6f62d758291033bef8d08
Kitvins <quirogakevin101@gmail.com>
2025-08-14 22:05:13 -0400

    guardar cambios antes de cambiar a master

--- practicas/p2-/Program.cs
+++ practicas/p2-/Program.cs
@@ -5,21 +5,17 @@ class Program
 {
     static void Main()
     {
-        // Pedir al usuario un número N
         Console.Write("Ingrese un número N: ");
         int N = int.Parse(Console.ReadLine());
 
         int suma = 0;
         int i = 1;
-
-        // Bucle while para sumar del 1 al N
         while (i <= N)
         {
             suma += i;
             i++;
         }
 
-        // Mostrar el resultado
         Console.WriteLine($"La suma de los números del 1 al {N} es: {suma}");
     }
 }
