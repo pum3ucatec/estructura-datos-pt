a22d014272d6bfdca850be7141bf49797dccb40d
THOORH <vicougarte12@gmail.com>
2025-08-17 13:58:52 -0400

    subo m tarea de la practica1

---
+++ practicas/p10-adivina-el-numero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int aleatorio = rnd.Next(1, 51);
+            int intento;
+
+            Console.WriteLine("Adivina el número (1-50):");
+
+            do
+            {
+                Console.Write("Tu intento: ");
+                intento = int.Parse(Console.ReadLine());
+
+                if (intento < aleatorio)
+                    Console.WriteLine("Más alto");
+                else if (intento > aleatorio)
+                    Console.WriteLine("Más bajo");
+
+            } while (intento != aleatorio);
+
+            Console.WriteLine("¡Correcto! El número era " + aleatorio);
+        }
+    }
+}
