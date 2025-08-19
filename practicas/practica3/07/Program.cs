85e000325c26c7d2773b08aa767916797927e244
Jheferson Rivera <jhersonriver @gmail.com>
2025-08-19 18:42:32 -0400

    Subiendo Practica 3

---
+++ practicas/practica3/07/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arreglo = { 2, 5, 2, 8, 2, 9 };
+int buscado = 2;
+int contador = 0;
+
+for (int i = 0; i < arreglo.Length; i++)
+{
+    if (arreglo[i] == buscado)
+        contador++;
+}
+
+Console.WriteLine("El número aparece " + contador + " veces");
