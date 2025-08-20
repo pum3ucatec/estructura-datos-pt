2ed8ebbe28bef4cf32ee0e52669fd2d92e2a35c8
camilanoe-wq <camilanoemamani@gmail.com>
2025-08-19 22:01:12 -0400

    Ejerccios Practica 3

---
+++ practicas/practica3/ej4-promedio-de-elementos/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ejercicio 4: Calcular el promedio de un arreglo
+// Camii
+using System;
+using System.Linq; // Necesario para usar .Sum()
+
+class Program
+{
+    static void Main()
+    {
+        // Arreglo con notas
+        int[] notas = { 80, 70, 90, 60, 100 };
+
+        // Calculamos promedio = suma de elementos / cantidad
+        double promedio = (double)notas.Sum() / notas.Length;
+
+        Console.WriteLine($"El promedio del numero es = {promedio}");
+    }
+}
+
