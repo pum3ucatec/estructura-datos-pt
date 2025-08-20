b6ce2bf4f8c855df6659fa19d762c568fa6a4fda
starlord1270 <lords2395@gmail.com>
2025-08-20 13:00:27 -0400

    subiendo mi practica 3

---
+++ practicas/practica3/ej4-promedio/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejercicio 4!");
+
+
+
+double[] calificaciones = new double[5];
+double suma = 0;
+
+
+for (int i = 0; i < calificaciones.Length; i++)
+{
+    Console.Write($"Ingrese la calificación {i + 1}: ");
+    calificaciones[i] = Convert.ToDouble(Console.ReadLine());
+    suma += calificaciones[i];
+}
+
+
+double promedio = suma / calificaciones.Length;
+
+
+Console.WriteLine("Notas: [" + string.Join(", ", calificaciones) + "]");
+Console.WriteLine($"Promedio: {promedio}");
