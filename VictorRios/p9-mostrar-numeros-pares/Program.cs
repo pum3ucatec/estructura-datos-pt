51794c3c2f0c3a0ec0f8e1039ea7388c7ea3ecbb
Victor Rios <vrios.01@outlook.com>
2025-08-20 12:44:42 -0400

    Wip: cambios antes del pull

---
+++ VictorRios/p9-mostrar-numeros-pares/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write($"Ingresa el valor: ");
+        int valor = int.Parse(Console.ReadLine());
+        Console.WriteLine("Primeros 10 números pares:");
+
+        for (int a = valor; a < valor + 10; a++)
+        {
+            int par = a * 2; // 2, 4, 6, ... 20
+            Console.WriteLine(par + (a < 10 ? " " : "\n"));
+        }
+    }
+}
+
