5ceb31c50bcc327b026dacc00d524020cd5f0856
Kitvins <quirogakevin101@gmail.com>
2025-08-22 12:49:07 -0400

    respuestas del examen

---
+++ examenes/examen-parcial/pregunta1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 4, 5, 6 };
+        int n = 6; // El valor máximo esperado
+
+        int sumaEsperada = n * (n + 1) / 2;
+        int sumaReal = 0;
+
+        foreach (int num in arr)
+        {
+            sumaReal += num;
+        }
+
+        int faltante = sumaEsperada - sumaReal;
+        Console.WriteLine($"Falta el número: {faltante}");
+    }
+}
+
---
+++ examenes/examen-parcial/pregunta2/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        List<int> resultado = RecorrerEspiral(matriz);
+
+        Console.WriteLine("Recorrido en espiral: ");
+        Console.WriteLine(string.Join(", ", resultado));
+    }
+
+    static List<int> RecorrerEspiral(int[,] matriz)
+    {
+        List<int> lista = new List<int>();
+
+        int filas = matriz.GetLength(0);
+        int columnas = matriz.GetLength(1);
+
+        int arriba = 0, abajo = filas - 1;
+        int izquierda = 0, derecha = columnas - 1;
+
+        while (arriba <= abajo && izquierda <= derecha)
+        {
+            for (int i = izquierda; i <= derecha; i++)
+                lista.Add(matriz[arriba, i]);
+            arriba++;
+
+            for (int i = arriba; i <= abajo; i++)
+                lista.Add(matriz[i, derecha]);
+            derecha--;
+
+            if (arriba <= abajo)
+            {
+                for (int i = derecha; i >= izquierda; i--)
+                    lista.Add(matriz[abajo, i]);
+                abajo--;
+            }
+            
+            if (izquierda <= derecha)
+            {
+                for (int i = abajo; i >= arriba; i--)
+                    lista.Add(matriz[i, izquierda]);
+                izquierda++;
+            }
+        }
+
+        return lista;
+    }
+}
