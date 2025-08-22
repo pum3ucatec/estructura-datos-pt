b916a477fa0452d1148ca0702520feb38912b7c6
Isabel <quisperamosisabelalejandra@gmail.com>
2025-08-22 12:28:44 -0400

    Subiendo una parte del examen

---
+++ examenes/examen-parcial/preguntta1/Program.cs
@@ -0,0 +1,2 @@
+﻿// Espiral en una Matriz Escribe un programa que recorra e imprima los elementos de una matriz en forma de espiral. 
+// Ejemplo: [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ] → [1, 2, 3, 6, 9, 8, 7, 4, 5].
