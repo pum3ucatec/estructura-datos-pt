83d213b1cdfcacb51d64b5f0e230c43c178ee73e
PaolaLV71 <paolalaure71@gmail.com>
2025-08-22 12:29:56 -0400

    Examen Parcial

---
+++ examenes/examen-parcial/pregunta1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace examen_parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+            List<int> resultado = RecorrerEspiral(matriz);
+
+            Console.WriteLine("Recorrido en espiral:");
+            Console.WriteLine(string.Join(", ", resultado));
+        }
+    }
+}
