0de135774ff364d54a5fcf1ef508b0a1b70bc3b0
estef3205 <estefy.peredo.anzoleaga@gmail.com>
2025-08-22 12:45:43 -0400

    Subiendo mi rama de examen

---
+++ examenes/examen-parcial/pregunta1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class pregunta1 {
+    static void Main() {
+        int[,] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int filaInicio = 0, filaFin = 2, colInicio = 0, colFin = 2;
+
+        Console.Write("Recorrido en espiral: ");
+        while (filaInicio <= filaFin && colInicio <= colFin) {
+            for (int i = colInicio; i <= colFin; i++) Console.Write(matriz[filaInicio, i] + " ");
+            filaInicio++;
+
+            for (int i = filaInicio; i <= filaFin; i++) Console.Write(matriz[i, colFin] + " ");
+            colFin--;
+
+            if (filaInicio <= filaFin) {
+                for (int i = colFin; i >= colInicio; i--) Console.Write(matriz[filaFin, i] + " ");
+                filaFin--;
+            }
+
+            if (colInicio <= colFin) {
+                for (int i = filaFin; i >= filaInicio; i--) Console.Write(matriz[i, colInicio] + " ");
+                colInicio++;
+            }
+        }
+    }
+}
---
+++ examenes/examen-parcial/pregunta2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class pregunta2 {
+    static void Main() {
+        int[] arr = {1, 2, 4, 5, 6};
+        int n = arr.Length + 1;
+        int sumaEsperada = n * (n + 1) / 2;
+        int sumaReal = 0;
+
+        foreach (int num in arr) sumaReal += num;
+
+        Console.WriteLine("Número faltante: " + (sumaEsperada - sumaReal));
+    }
+}
