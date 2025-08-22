2fa477ba39cf339017f90a1d80ae309ee35b3f35
Victor Rios <vrios.01@outlook.com>
2025-08-22 12:58:08 -0400

    pregunta uno y dos

--- examenes/examen-parcial/pregunta-1/Program.cs
+++ examenes/examen-parcial/pregunta-1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static int EncontrarNumeroFaltante(int[] arreglo, int n)
+    {
+        // Suma total de los números del 1 al n
+        int sumaTotal = n * (n + 1) / 2;
+
+        // Suma de los elementos del arreglo
+        int sumaArreglo = 0;
+        foreach (int num in arreglo)
+        {
+            sumaArreglo += num;
+        }
+
+        // El número faltante es la diferencia
+        return sumaTotal - sumaArreglo;
+    }
+
+    static void Main()
+    {
+        int[] arreglo = { 1, 2, 4, 5, 6 };
+        int n = 6;
+
+        int faltante = EncontrarNumeroFaltante(arreglo, n);
+        Console.WriteLine($"El número que falta es: {faltante}");
+    }
+}
+
--- examenes/examen-parcial/pregunta-2/Program.cs
+++ examenes/examen-parcial/pregunta-2/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<int> RecorrerEspiral(int[,] matriz)
+    {
+        List<int> resultado = new List<int>();
+
+        int filas = matriz.GetLength(0);
+        int columnas = matriz.GetLength(1);
+
+        int inicioFila = 0, finFila = filas - 1;
+        int inicioCol = 0, finCol = columnas - 1;
+
+        while (inicioFila <= finFila && inicioCol <= finCol)
+        {
+            // Recorrer la fila superior de izquierda a derecha
+            for (int col = inicioCol; col <= finCol; col++)
+            {
+                resultado.Add(matriz[inicioFila, col]);
+            }
+            inicioFila++;
+
+            // Recorrer la columna derecha de arriba a abajo
+            for (int fila = inicioFila; fila <= finFila; fila++)
+            {
+                resultado.Add(matriz[fila, finCol]);
+            }
+            finCol--;
+
+            if (inicioFila <= finFila)
+            {
+                // Recorrer la fila inferior de derecha a izquierda
+                for (int col = finCol; col >= inicioCol; col--)
+                {
+                    resultado.Add(matriz[finFila, col]);
+                }
+                finFila--;
+            }
+
+            if (inicioCol <= finCol)
+            {
+                // Recorrer la columna izquierda de abajo a arriba
+                for (int fila = finFila; fila >= inicioFila; fila--)
+                {
+                    resultado.Add(matriz[fila, inicioCol]);
+                }
+                inicioCol++;
+            }
+        }
+
+        return resultado;
+    }
+
+    static void Main()
+    {
+        int[,] matriz = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        List<int> espiral = RecorrerEspiral(matriz);
+
+        Console.WriteLine("Elementos en espiral:");
+        Console.WriteLine(string.Join(", ", espiral));
+    }
+}
+
