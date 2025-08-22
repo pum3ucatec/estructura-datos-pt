d27cfa012090b4e8c273b794b38de29848b0694a
Isabel <quisperamosisabelalejandra@gmail.com>
2025-08-22 05:50:51 -0400

    Subiendo mi Practica5

---
+++ practicas/practica5/ejer5-interseccion-de-dos-arreglos/Program.cs
@@ -0,0 +1,30 @@
+﻿// Dado dos arreglos de enteros, encuentra los elementos comunes sin usar LINQ.  
+//Ejemplo: `[1,2,2,1]` y `[2,2]` → `[2,2]`.
+using System;
+
+class Ejercicio5
+{
+    static void Main()
+    {
+        int[] arreglo1 = { 1, 2, 2, 1, 3, 3 };
+        int[] arreglo2 = { 1, 1, 3, 3 };
+
+        Console.WriteLine("Arreglos originales:");
+        Console.WriteLine(string.Join(", ", arreglo1)); 
+        Console.WriteLine(string.Join(", ", arreglo2)); 
+        Console.WriteLine("Numeros iguales:");
+
+        for (int i = 0; i < arreglo1.Length; i++)
+        {
+            for (int j = 0; j < arreglo2.Length; j++)
+            {
+                if (arreglo1[i] == arreglo2[j])
+                {
+                    Console.Write(arreglo1[i] + " ");
+                    arreglo2[j] = int.MinValue;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ practicas/practica5/ejer7-arreglo-de-productos/Program.cs
@@ -0,0 +1,37 @@
+﻿// Dado un arreglo de enteros, construye un nuevo arreglo donde cada posición contenga el producto de 
+// todos los elementos excepto el de esa posición (sin usar división).  
+/* el codigo fuunciona asi:
+En la posición 0: 4 × 5 × 11 = 220
+En la posición 1: 12 × 5 × 11 = 660
+En la posición 2: 12 × 4 × 11 = 528
+En la posición 3: 12 × 4 × 5 = 240 */
+
+using System;
+
+class Ejercico7 {
+    static void Main() {
+        int[] arreglo = { 12, 4, 5, 11 };
+        int n = arreglo.Length;
+
+        int[] resultado = new int[n];
+        int[] izquierda = new int[n];
+        int[] derecha = new int[n];
+
+        izquierda[0] = 1;
+        for (int i = 1; i < n; i++) {
+            izquierda[i] = izquierda[i - 1] * arreglo[i - 1];
+        }
+
+        derecha[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            derecha[i] = derecha[i + 1] * arreglo[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            resultado[i] = izquierda[i] * derecha[i];
+        }
+
+        Console.WriteLine("Resultado: " + string.Join(", ", resultado));
+    }
+}
+
