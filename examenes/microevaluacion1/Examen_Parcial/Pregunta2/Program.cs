91acea3aa15ad465db327e4baad26813a61ea748
Alvaro1672 <sahoneroalvaroogmail.com>
2025-08-22 12:57:41 -0400

    Subiendo los cambios de la pregunta2

---
+++ examenes/microevaluacion1/Examen_Parcial/Pregunta2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Encontrar el Número que Falta En un arreglo de números del 1 al n, falta un número. Encuentra cuál es. Ejemplo: [1, 2, 4, 5, 6] → Falta 3.
+
+
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 9 };
+        int n = arr.Length + 1;
+        int sumaTotal = n * (n + 1) / 2;
+        int sumaArr = 0;
+
+        foreach (int num in arr)
+        {
+            sumaArr += num;
+        }
+
+        int faltante = sumaTotal - sumaArr;
+        Console.WriteLine("El número faltante es: " + faltante);
+ 
