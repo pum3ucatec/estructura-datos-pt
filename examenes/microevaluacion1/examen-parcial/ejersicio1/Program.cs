5f6704e9d766af4c4732cbf7e3c1c53ee699381e
THOORH <vicougarte12@gmail.com>
2025-08-22 12:46:38 -0400

    subiendo mi primer examen parcial

---
+++ examenes/microevaluacion1/examen-parcial/ejersicio1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ejersicio1
+{
+    static void Main(string[] args)
+    {
+        int[,] matriz = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        List<int> resultado = RecorrerEspiral(matriz);
+
+        Console.WriteLine("Recorrido en espiral:");
+        Console.WriteLine(string.Join(", ", resultado));
+    }
+
+    static List<int> RecorrerEspiral(int[,] matriz)
+    {
+        List<int> resultado = new List<int>();
+
+        int arriba = 0, 
+            abajo = matriz.GetLength(0) - 1;
+        int izquierda = 0, 
+            derecha = matriz.GetLength(1) - 1;
+
+        while (arriba <= abajo && izquierda <= derecha)
+        {
+           
+            for (int i = izquierda; i <= derecha; i++)
+                resultado.Add(matriz[arriba, i]);
+            arriba++;
+
+        
+            for (int i = arriba; i <= abajo; i++)
+                resultado.Add(matriz[i, derecha]);
+            derecha--;
+
+            if (arriba <= abajo)
+            {
+            
+                for (int i = derecha; i >= izquierda; i--)
+                    resultado.Add(matriz[abajo, i]);
+                abajo--;
+            }
+
+            if (izquierda <= derecha)
+            {
+               
+                for (int i = abajo; i >= arriba; i--)
+                    resultado.Add(matriz[i, izquierda]);
+                izquierda++;
+            }
+        }
+
+        return resultado;
+    }
+}
+
---
+++ examenes/microevaluacion1/examen-parcial/ejersicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ejersicio2
+{
+    static void Main(string[] args)
+    {
+        int[] arreglo = { 1, 2, 4, 5, 6 };  // Falta el 3
+        int n = arreglo.Length + 1; // porque falta un número
+
+        int sumaEsperada = n * (n + 1) / 2;
+        int sumaActual = 0;
+
+        foreach (int num in arreglo)
+        {
+            sumaActual += num;
+        }
+
+        int numeroFaltante = sumaEsperada - sumaActual;
+
+        Console.WriteLine("El número que falta es el : " + numeroFaltante);
+    }
+}
