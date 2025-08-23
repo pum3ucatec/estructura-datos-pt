453dff8d0ac8d0a0847990c45972e57cf72168ea
starlord1270 <lords2395@gmail.com>
2025-08-23 13:27:54 -0400

    agregando la practica 5

---
+++ practicas/practica5/ej1-rotacion-arreglo/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Ejercicio 1! Rotación de Arreglo");
+
+static void RotarArreglo(int[] arr, int k)
+{
+    int n = arr.Length;
+    if (n == 0) return;
+
+    k = k % n;
+
+    int[] resultado = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        int nuevaPos = (i + k) % n;
+        resultado[nuevaPos] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = resultado[i];
+    }
+}
+
+
+int[] arreglo = { 1, 2, 3, 4, 5 };
+int k = 2;
+
+Console.WriteLine("Arreglo original: " + string.Join(", ", arreglo));
+
+RotarArreglo(arreglo, k);
+
+Console.WriteLine($"Arreglo rotado {k} posiciones a la derecha: " + string.Join(", ", arreglo));
