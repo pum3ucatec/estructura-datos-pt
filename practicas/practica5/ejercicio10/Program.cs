f2b81c1ff64eab99ceae07c1cb050000a5bdf2a7
LunaDidi23 <lauradiana7620@gmail.com>
2025-08-26 11:12:33 -0400

    Subiendo mi practica 5

---
+++ practicas/practica5/ejercicio10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio10
+{
+    static void Main()
+    {
+        int[] arr = {2,3,6,7};
+        int target = 7;
+        List<int> temp = new List<int>();
+        Subsets(arr, target, 0, temp, 0);
+    }
+
+    static void Subsets(int[] arr, int target, int suma, List<int> temp, int start)
+    {
+        if (suma == target)
+        {
+            Console.Write("[");
+            for(int i=0;i<temp.Count;i++)
+            {
+                Console.Write(temp[i]);
+                if(i < temp.Count-1) Console.Write(", ");
+            }
+            Console.WriteLine("]");
+            return;
+        }
+        if (suma > target) return;
+
+        for(int i=start;i<arr.Length;i++)
+        {
+            temp.Add(arr[i]);
+            Subsets(arr, target, suma+arr[i], temp, i); // permitir repetir
+            temp.RemoveAt(temp.Count-1);
+        }
+    }
+}
---
+++ practicas/practica5/ejercicio6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Ejercicio6
+{
+    static void Main()
+    {
+        int[,] mat = { {1,2,3}, {4,5,6} };
+        int filas = mat.GetLength(0);
+        int col = mat.GetLength(1);
+
+        int[,] trans = new int[col, filas];
+
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                trans[j,i] = mat[i,j];
+            }
+        }
+
+        Console.WriteLine("Matriz transpuesta:");
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < filas; j++)
+                Console.Write(trans[i,j] + " ");
+            Console.WriteLine();
+        }
+    }
+}
