840a676e647abc989db4ae884607c9d071a61f14
starlord1270 <lords2395@gmail.com>
2025-08-29 12:25:01 -0400

    subiendo la microevaluacion

--- examenes/micro-evaluacion2/ej1-insertar-al-inicio/Program.cs
+++ examenes/micro-evaluacion2/ej1-insertar-al-inicio/Program.cs
@@ -1,18 +1,66 @@
 ﻿using System;
-using System;
+Console.WriteLine("Micro Evaluacion 2 - Ejercicio 1 insertar una lista simple");
 
 public class Nodo()
 {
     int siguiente;
     int anterior;
     int contenedor;
-
     string valor;
 }
 
-public class pila()
+public class Pila()
 {
-    frente
+    int frente = 0;
+    int siguiente = ++1;
+    int final = null;
 }
 
-Console.WriteLine("Micro Evaluacion 2");
+public void push
+    {
+if (frente <= 0)
+{
+            Console.WriteLine("la pila esta vacia");
+        }
+else
+{
+            valor.push();
+        }
+
+    }
+
+
+    public void principal
+    {
+    int opcion; 
+     
+     do
+     {
+            if (opcion != opcion ConsoleRedline.TryParse()out )
+     } while (true);
+    }
+-----------------------------
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int opciones;
+        int numeros;
+        contenedor = personajes;
+
+        while (contenedor! > 0)
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("ingresa un elemento en la cola");
+            Console.WriteLine("Salir del programa");
+
+            switch{
+        case1:
+            if (opciones = 1)
+                Console.WriteLine("ingresa un elemento a la cola");
+            Console.Readline();
+            numeros.push();
+            breack;
+        }
+    }
+}
