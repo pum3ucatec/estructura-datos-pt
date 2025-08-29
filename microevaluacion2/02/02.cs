49f3a47989d01017c6f99f2ad33ef40809d2fbd1
unknown <jhersonriver@gmail.com>
2025-08-29 12:28:09 -0400

    Subiendo laMicroEvaluación 2

---
+++ microevaluacion2/02/02.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+
+public class Nodo<T>
+{
+    public T Dato { get; set; }
+    public Nodo<T> Siguiente { get; set; }
+
+    public Nodo(T dato)
+    {
+        Dato = dato;
+        Siguiente = null;
+    }
+}
+
+public class ListaSimple<T>
+{
+    private Nodo<T> primero;
+
+    public void Agregar(T dato)
+    {
+        Nodo<T> nuevo = new Nodo<T>(dato);
+        if (primero == null)
+        {
+            primero = nuevo;
+        }
+        else
+        {
+            Nodo<T> actual = primero;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevo;
+        }
+    }
+
+    public void Mostrar()
+    {
+        Nodo<T> actual = primero;
+        while (actual != null)
+        {
+            Console.WriteLine(actual.Dato);
+            actual = actual.Siguiente;
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        ListaSimple<string> lista = new ListaSimple<string>();
+        lista.Agregar("Anthony");
+        lista.Agregar("Patrick");
+        lista.Agregar("Kevin");
+        lista.Agregar("Dianna");
+
+        lista.Mostrar();
+        EliminarAlrededor(lista, "Dianna");
+        lista.Mostrar();
+    }
+}
+
+public static void EliminarAlrededor(ListaSimple<string> lista, string nombre)
+{
+    Nodo<string> actual = lista.GetPrimero();
+    Nodo<string> anterior = null;
+    Nodo<string> previoAnterior = null;
+
+    while (actual != null)
+    {
+        if (actual.Dato == nombre)
+        {
+          
+            if (anterior != null)
+            {
+                if (previoAnterior == null)
+                {
+                    lista.SetPrimero(actual);
+                }
+                else
+                {
+                    previoAnterior.Siguiente = actual;
+                }
+            }
+
+        
+            if (actual.Siguiente != null)
+            {
+                actual.Siguiente = actual.Siguiente.Siguiente;
+            }
+            break;
+        }
+        previoAnterior = anterior;
+        anterior = actual;
+        actual = actual.Siguiente;
+    }
+}
+
+
+public partial class ListaSimple<T>
+{
+    public Nodo<T> GetPrimero()
+    {
+        return primero;
+    }
+
+    public void SetPrimero(Nodo<T> nodo)
+    {
+        primero = nodo;
+    }
+}
+
+EliminarAlrededor(lista, "Dianna");
+lista.Mostrar();
+
+
