9f7a4face041bf7cf601451a533fe9dbee241de8
Alvaro1672 <sahoneroalvaroogmail.com>
2025-09-02 10:39:36 -0400

    Verificando cambios de mi Readme

---
+++ practicas/practica1/Ej.3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+namespace Tabla1al10
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Elige una tabla de multiplicar: ");
+            int table = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Tabla del {table}");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = table * i; 
+                Console.WriteLine($"{table} x {i} = {resultado}");
+            }
+
+        }
+    }
+        
+}
---
+++ practicas/practica1/Ej.4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MenuDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. SUMA");
+                Console.WriteLine("2. RESTA");
+                Console.WriteLine("3. MULTIPLICACION");
+                Console.WriteLine("4. Salir");
+                Console.Write("Selecciona una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        // Lógica para la suma
+                        Console.Write("Ingrese el primer número: ");
+                        int num1 = int.Parse(Console.ReadLine());
+                        Console.Write("Ingrese el segundo número: ");
+                        int num2 = int.Parse(Console.ReadLine());
+                        int suma = num1 + num2;
+                        Console.WriteLine($"La suma es: {suma}");
+                        break;
+                    case 2:
+                        // Lógica para la resta
+                        Console.Write("Ingrese el primer número: ");
+                        int num3 = int.Parse(Console.ReadLine());
+                        Console.Write("Ingrese el segundo número: ");
+                        int num4 = int.Parse(Console.ReadLine());
+                        int resta = num3 - num4;
+                        Console.WriteLine($"La resta es: {resta}");
+                        break;
+                    case 3:
+                        // Lógica para la multiplicación
+                        Console.Write("Ingrese el primer número: ");
+                        int num5 = int.Parse(Console.ReadLine());
+                        Console.Write("Ingrese el segundo número: ");
+                        int num6 = int.Parse(Console.ReadLine());
+                        int multiplicacion = num5 * num6;
+                        Console.WriteLine($"La multiplicación es: {multiplicacion}");
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida. Intenta de nuevo.");
+                        break;
+                }
+
+            } while (opcion != 4);
+        }
+    }
+}
