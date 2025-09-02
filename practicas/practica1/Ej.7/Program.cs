45a7ae7d45bde939b794845812c2984459436048
Alvaro1672 <sahoneroalvaroogmail.com>
2025-09-02 12:10:06 -0400

    Error

--- practicas/practica1/Ej.7/Program.cs
+++ practicas/practica1/Ej.7/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Suma_de_7_Valores
+{
+    class Program
+    {
+
+        static void Main()
+        {
+
+            int suma = 0;
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.Write($"Ingresa el valor {i}: ");
+                int valor = int.Parse(Console.ReadLine());
+                suma += valor;
+
+            }
+            Console.WriteLine($"la suma de los 7 valores es {suma}");
+        }
+    }
+
+
+}
