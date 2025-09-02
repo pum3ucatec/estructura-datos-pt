fd262851bedbc18d098f4a5bd19fd5a54ecccd63
estef3205 <estefy.peredo.anzoleaga@gmail.com>
2025-09-02 14:56:41 -0400

    Arreglando estilo de nombrado

---
+++ practicas/practica5/ej7-arreglo-productos-sin-indice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Ejercicio7 {
+    static void Main() {
+        int[] arreglo = {1, 2, 3, 4};
+        int n = arreglo.Length;
+        int[] izquierda = new int[n];
+        int[] derecha = new int[n];
+        int[] resultado = new int[n];
+
+        izquierda[0] = 1;
+        for (int i = 1; i < n; i++) izquierda[i] = izquierda[i - 1] * arreglo[i - 1];
+
+        derecha[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) derecha[i] = derecha[i + 1] * arreglo[i + 1];
+
+        for (int i = 0; i < n; i++) resultado[i] = izquierda[i] * derecha[i];
+
+        Console.WriteLine("Resultado: " + string.Join(", ", resultado));
+    }
+}
---
+++ practicas/practica5/ej8-buscar-elemento-arreglo-rotado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Ejercicio8 {
+    static int Buscar(int[] arreglo, int objetivo) {
+        int inicio = 0, fin = arreglo.Length - 1;
+        while (inicio <= fin) {
+            int mid = (inicio + fin) / 2;
+            if (arreglo[mid] == objetivo) return mid;
+
+            if (arreglo[inicio] <= arreglo[mid]) {
+                if (objetivo >= arreglo[inicio] && objetivo < arreglo[mid]) fin = mid - 1;
+                else inicio = mid + 1;
+            } else {
+                if (objetivo > arreglo[mid] && objetivo <= arreglo[fin]) inicio = mid + 1;
+                else fin = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    static void Main() {
+        int[] arreglo = {4,5,6,7,0,1,2};
+        int objetivo = 0;
+        Console.WriteLine("Índice encontrado: " + Buscar(arreglo, objetivo));
+    }
+}
