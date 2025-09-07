ec0d44aa1588ac098cc963639b838d5bc1bae108
nicolaslavadenzhervas-create <nicolaslavadenzhervas@gmail.com>
2025-09-07 01:11:51 -0400

    Subiendo Practica 4

---
+++ practicas/practica4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingresa un número decimal:");
+        int numero = int.Parse(Console.ReadLine());
+
+        Stack<int> pila = new Stack<int>();
+
+        int n = numero;
+
+        // Divisiones sucesivas entre 2
+        while (n > 0)
+        {
+            int residuo = n % 2; // obtenemos el bit
+            pila.Push(residuo);  // se apila
+            n = n / 2;           // se divide entre 2
+        }
+
+        Console.WriteLine($"Número decimal: {numero}");
+        Console.Write("Número binario: ");
+
+        // Desapilar para formar el binario
+        while (pila.Count > 0)
+        {
+            Console.Write(pila.Pop());
+        }
+
+        Console.WriteLine();
+    }
+}
